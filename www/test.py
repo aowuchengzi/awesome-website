1ce1c8b61e14ec04c83eb943ff90c5f912d4e58b
aowuchengzi <1533971306@qq.com>
2019-07-01 10:40:54 +0800

    add

--- www/test.py
+++ www/test.py
@@ -4,7 +4,7 @@ from models import User, Blog, Comment
 
 async def test(loop):
     await orm.create_pool(loop=loop, user='root', password='yc199510.', db='awesome')
-    u = User(name='Test2', email='test2@qq.com', passwd='123456',image='about:blank')
+    u = User(name='aowuchengzi', email='1533971306@qq.com', passwd='yc199510.',image='about:blank', admin=True)
     await u.save()
     ## 添加到数据库后需要关闭连接池，否则会报错 RuntimeError: Event loop is closed
     orm.__pool.close()
